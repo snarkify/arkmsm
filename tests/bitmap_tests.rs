ab52a47d198cce6a58809d996e26f5e032dc448d
cyphersnake <cyphersnake@pm.me>
2023-08-15 12:05:00 +0400

    Refactor with `assert_false!` & `assert_lt!`
    With the `all_asserts` crate, a slightly more intuitive `assert` is made.
    In addition to easier readability of `assert_false`, `assert_lt` outputs the values that were compared by default, which makes debugging easier.

--- tests/bitmap_tests.rs
+++ tests/bitmap_tests.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod bitmap_tests {
+    use all_asserts::assert_false;
     use ark_msm::bitmap::Bitmap;
 
     fn setup() -> Bitmap {
@@ -9,24 +10,24 @@ mod bitmap_tests {
     #[test]
     fn test_set_when_only_once_return_false() {
         let mut bitmap = setup();
-        assert!(!bitmap.test_and_set(0));
-        assert!(!bitmap.test_and_set(33));
+        assert_false!(bitmap.test_and_set(0));
+        assert_false!(bitmap.test_and_set(33));
     }
 
     #[test]
     fn test_set_when_more_than_once_return_true() {
         let mut bitmap = setup();
-        assert!(!bitmap.test_and_set(0));
+        assert_false!(bitmap.test_and_set(0));
         assert!(bitmap.test_and_set(0));
     }
 
     #[test]
     fn test_clear() {
         let mut bitmap = setup();
-        assert!(!bitmap.test_and_set(0));
+        assert_false!(bitmap.test_and_set(0));
         assert!(bitmap.test_and_set(0));
 
         bitmap.clear();
-        assert!(!bitmap.test_and_set(0));
+        assert_false!(bitmap.test_and_set(0));
     }
 }
